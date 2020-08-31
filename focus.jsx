68d94c539f64b9c46e942bdde013a470b56698ab
VrishabKrishna <krishnavrishab@gmail.com>
2020-08-31 19:53:12 +0530

    feat: focused application

---
+++ focus.jsx
@@ -0,0 +1,34 @@
+import { config } from "./config.jsx";
+
+export const command =
+  "osascript -e 'tell application \"System Events\"' -e 'set frontApp to name of first application process whose frontmost is true' -e 'end tell'";
+
+export const refreshFrequency = 100;
+
+export const style = {
+  ...config,
+  paddingRight: "20px",
+  paddingLeft: "20px",
+  cursor: "default",
+  position: "fixed",
+  zIndex: "-1",
+  left: "4%",
+  userSelect: "none",
+  overflow: "visible",
+};
+
+export const render = (output) => {
+  let dt = output.output;
+  console.log({ dt });
+  return (
+    <div style={style}>
+      <span
+        style={{
+          color: "rgb(205, 205, 205)",
+        }}
+      >
+        {dt}
+      </span>
+    </div>
+  );
+};
