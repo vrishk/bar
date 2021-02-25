e69355a3291b33358309bba8a70a0482009e471b
vrishk <krishnavrishab@gmail.com>
2021-02-25 21:58:35 +0530

    fix: wifi not showing

--- scripts/get_wifi.sh
+++ scripts/get_wifi.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
 
-set -Eeuo pipefail
-
-
 WIFI_STATUS=$(ifconfig en0 | grep status | cut -c 10-)
 WIFI_SSID=$(networksetup -getairportnetwork en0 | cut -c 24-)
 
-ETHERNET_STATUS=$(ifconfig en8 | grep status | cut -c 10-)
+ETHERNET_STATUS=$(ifconfig en8 | grep status | cut -c 10-) || "inactive"
 
 
 echo $(cat <<-EOF
