8f90f18ea1213bdedc595a64fc34296c1cdaabc0
vrishk <krishnavrishab@gmail.com>
2021-04-28 12:19:06 +0530

    fix: general ethernet

--- scripts/get_wifi.sh
+++ scripts/get_wifi.sh
@@ -3,8 +3,7 @@
 WIFI_STATUS=$(ifconfig en0 | grep status | cut -c 10-)
 WIFI_SSID=$(networksetup -getairportnetwork en0 | cut -c 24-)
 
-ETHERNET_STATUS=$(ifconfig en8 | grep status | cut -c 10-) || "inactive"
-
+ETHERNET_STATUS=$(ifconfig en8 | grep status | cut -c 10- && ifconfig en6 | grep status | cut -c 10- || echo "inactive")
 
 echo $(cat <<-EOF
   {
