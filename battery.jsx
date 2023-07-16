e06c3d7e186eda6f2f9ee7e57f73c4d30cf5fb91
vrishk <krishnavrishab@gmail.com>
2023-07-16 11:02:50 -0700

    fix: add try catch to battery

--- battery.jsx
+++ battery.jsx
@@ -17,26 +17,27 @@ export const style = {
 };
 
 export const render = (output) => {
-  console.log("battery", output);
-  let [level, status] = output.output.split("; ");
-  let charging = status !== "discharging\n";
-  let color = "rgba(0, 205, 0, 1.0)";
-  if (parseInt(level) < 15 && charging == false) {
-    color = "rgba(255, 0, 0, 1.0)";
-  } else if (parseInt(level) < 30 && charging == false) {
-    color = "rgba(255, 205, 1.0)";
-  }
-
-  return (
-    <div style={style}>
-      <span
-        style={{
-          color: color,
-        }}
-      >
-        {charging ? "⚡" : null}
-        {level}
-      </span>
-    </div>
-  );
+  try {
+    console.log("battery", output);
+    let [level, status] = output.output.split("; ");
+    let charging = status !== "discharging\n";
+    let color = "rgba(0, 205, 0, 1.0)";
+    if (parseInt(level) < 15 && charging == false) {
+      color = "rgba(255, 0, 0, 1.0)";
+    } else if (parseInt(level) < 30 && charging == false) {
+      color = "rgba(255, 205, 1.0)";
+    }
+    return (
+      <div style={style}>
+        <span
+          style={{
+            color: color,
+          }}
+        >
+          {charging ? "⚡" : null}
+          {level}
+        </span>
+      </div>
+    );
+  } catch {}
 };
